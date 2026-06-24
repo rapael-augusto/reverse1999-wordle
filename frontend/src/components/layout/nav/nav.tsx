a7c7b20805b9d499015d17ee66900b2c1024f780
Raphael Augusto <augustorapha27@gmail.com>
2026-06-24 13:19:35 -0300

    refactor: turning the nav into a component

---
+++ frontend/src/components/layout/nav/nav.tsx
@@ -0,0 +1,32 @@
+import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
+import "./nav.css"
+
+export default function Navbar() {
+  const { t } = useTranslation();
+
+  return (
+    <nav className="mode-navigation-wrapper">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "active" : "unactive")}
+      >
+        {t("nav.daily")}
+      </NavLink>
+
+      <NavLink
+        to="/unlimited"
+        className={({ isActive }) => (isActive ? "active" : "unactive")}
+      >
+        {t("nav.unlimited")}
+      </NavLink>
+
+      <NavLink
+        to="/all-characters"
+        className={({ isActive }) => (isActive ? "active" : "unactive")}
+      >
+        {t("nav.allCharacters")}
+      </NavLink>
+    </nav>
+  );
+}
