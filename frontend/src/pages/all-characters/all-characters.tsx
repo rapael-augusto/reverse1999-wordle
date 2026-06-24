dd688c260fece9fe55924a87c1a7eaa147ac0889
Raphael Augusto <augustorapha27@gmail.com>
2026-06-24 13:24:41 -0300

    feat: initial 'all-characters' and 'unlimited' pages

---
+++ frontend/src/pages/all-characters/all-characters.tsx
@@ -0,0 +1,11 @@
+import "./all-characters.css";
+import Navbar from "../../components/layout/nav/nav";
+
+export default function AllCharacters() {
+  return (
+    <>
+      <Navbar />
+      <h1>Coming Soon...</h1>
+    </>
+  );
+}
---
+++ frontend/src/pages/unlimited/unlimited.tsx
@@ -0,0 +1,11 @@
+import "./unlimited.css";
+import Navbar from "../../components/layout/nav/nav";
+
+export default function Unlimited() {
+  return (
+    <>
+      <Navbar />
+      <h1>Coming Soon...</h1>
+    </>
+  );
+}
