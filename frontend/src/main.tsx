88583f69cbeb93d39a3f3b94fab9663788405ed1
Raphael Augusto <augustorapha27@gmail.com>
2026-06-24 13:23:29 -0300

    feat: adding router logic to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './app/App.tsx'
 import './services/i18.ts'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router.tsx";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
