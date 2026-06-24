0917df6ed05c2d8a91742257f3130c0f908b4d9c
Raphael Augusto <augustorapha27@gmail.com>
2026-06-24 12:14:50 -0300

    feat: translation feature using i18n

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,26 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locales/en_translation.json";
+import pt from "./locales/pt_translation.json";
+
+const browserLanguage = navigator.language.slice(0, 2);
+
+i18next.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
+    },
+    pt: {
+      translation: pt,
+    },
+  },
+
+  lng: ["en", "pt"].includes(browserLanguage) ? browserLanguage : "en",
+  fallbackLng: "en",
+
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18next;
